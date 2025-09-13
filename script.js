2df18a840ae98d6bf543f1ddda23d60730559fa1
envwx <naz66cod@gmail.com>
2025-09-13 11:33:35 +0300

    added consts to the js

---
+++ script.js
@@ -0,0 +1,9 @@
+//consts
+const letters = document.querySelectorAll('.letters')
+const wordUnderscores = document.querySelector('#word-underscores')
+const livesElement = document.querySelector('.lives')
+const image = document.querySelector('#hangman-img')
+const gameOverContainer = document.querySelector('#game-over')
+const gameWinContainer = document.querySelector('#game-win')
+const resetGameBtn = document.querySelector('.reset')
+const hintText = document.querySelector('#hints')
